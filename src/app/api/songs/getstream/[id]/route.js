470cea0a76d62f16726c3851d8ebfbd6a44519b8
shibam-naskar <shibamnskar@gmail.com>
2023-11-06 18:08:01 +0530

    new fix

--- src/app/api/songs/getstream/[id]/route.js
+++ src/app/api/songs/getstream/[id]/route.js
@@ -4,23 +4,36 @@ const youtubedl = require('youtube-dl-exec')
 const fs = require('fs')
 const ytdl = require('ytdl-core')
 
+
+
 export async function GET(request, content) {
-    var url = await getAudioLink(`https://www.youtube.com/watch?v=${content.params.id}`)
-    return NextResponse.json(url)
-
-}
-
-async function getAudioLink(url) {
     try {
-        const info = await ytdl.getInfo(url);
-        const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'audioonly' });
-        if (audioFormat && audioFormat.url) {
-            return (audioFormat.url)
-        } else {
-            return 'Audio format not found';
-        }
+      const url = await getAudioLink(`https://www.youtube.com/watch?v=${content.params.id}`, 'http://your-proxy-url:your-proxy-port');
+      return NextResponse.json(url);
     } catch (error) {
-        console.error('Error getting audio link:', error);
-        return error
+      console.error('Error getting audio link:', error);
+      return NextResponse.error(error.message, 500);
     }
-}
+  }
+  
+  async function getAudioLink(url, proxyUrl) {
+    try {
+      const options = {
+        proxy: proxyUrl,
+        quality: 'highestaudio',
+        filter: 'audioonly',
+      };
+  
+      const info = await ytdl.getInfo(url, options);
+      const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'audioonly' });
+  
+      if (audioFormat && audioFormat.url) {
+        return audioFormat.url;
+      } else {
+        throw new Error('Audio format not found');
+      }
+    } catch (error) {
+      console.error('Error getting audio link:', error);
+      throw error;
+    }
+  }
